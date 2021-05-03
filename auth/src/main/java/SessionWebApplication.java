d2f017d55e0e9505679922d117c4f98e19ce9141
Ruleton-at-301701292774 <catalex777@gmail.com>
2021-05-03 17:01:54 +0300

    Homework_03

---
+++ auth/src/main/java/SessionWebApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SessionWebApplication.class, args);
+    }
+}
